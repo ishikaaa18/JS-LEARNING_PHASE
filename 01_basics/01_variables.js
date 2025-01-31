0c2e1faec1d562b4783117dc7e509c6cd04638b6
ishikaaa18 <166376072+ishikaaa18@users.noreply.github.com>
2025-01-31 16:33:34 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=12445
+let accountEmail="ishika@google.com"
+var accountPassword="1234"
+accountCity ="Agartala"
+let accountState;
+
+/*const and let is always preffered and if any variable is not declared that it shows undefined
+console.table prints multiple values
+*/
+accountEmail="ishk@gmail.com"
+accountPassword="734979"
+accountCity="dharmanagar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
